3c453b1fc4dca4c3a3101eedc4e16eee86257f2a
Toasty <louscliff@gmail.com>
2024-05-08 02:09:32 +0100

    - Remove old method.

--- Tokenizer.cs
+++ Tokenizer.cs
@@ -64,46 +64,6 @@
 			return false;
 		}
 
-		public static List<Token> GetTokensFromText(string codeAsText)
-		{
-			var tokens = new List<Token>();
-
-			for (int i = 0; i < codeAsText.Length; i++)
-			{
-				var character = codeAsText[i];
-				var text = character.ToString();
-
-				if (character == ' ')
-					continue;
-
-				Token token;
-
-				if (char.IsNumber(character))
-				{
-					token = ProcessNumber(codeAsText, ref i);
-				}
-				else if (char.IsLetter(character))
-				{
-					token = ProcessFunction(codeAsText, ref i);
-				}
-				else
-				{
-					token = character switch
-					{
-						'+' => new(TokenType.Add, text),
-						'-' => new(TokenType.Subtract, text),
-						'*' => new(TokenType.Multiply, text),
-						'/' => new(TokenType.Divide, text),
-						'(' => new(TokenType.OpenParentheses, text),
-						')' => new(TokenType.CloseParentheses, text),
-						_ => new(TokenType.Invalid, text),
-					};
-				}
-				tokens.Add(token);
-			}
-			return tokens;
-		}
-
 		private static Token ProcessNumber(string codeAsText, ref int index)
 		{
 			var numberText = string.Empty;
